72390b4f9ee171bab3b4e1107963a049e5928188
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-10-28 01:08:23 +0800

    feat: animate plane

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,4 +1,12 @@
+uniform float time;
+varying float pulse;
+
+varying vec2 vUv;
+
 void main () {
-  gl_Position = projectionMatrix *
-  modelViewMatrix * vec4( position, 1.0);
+  vUv = uv;
+  vec3 newPosition = position;
+  newPosition.z = 0.05*sin(length(position)*30. + time);
+  pulse = 20.*newPosition.z;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
 }
