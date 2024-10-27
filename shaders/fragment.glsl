ce8940fde73d41ab4f60cfc2e3e87a6a8401fbfb
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-10-28 01:29:42 +0800

    fix: decrease distortion value

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -5,7 +5,7 @@ varying float pulse;
 varying vec2 vUv; 
 
 void main () {
-  vec4 myImage = texture(uTexture, vUv + 0.1*sin(vUv*20. - time));
+  vec4 myImage = texture(uTexture, vUv + 0.01*sin(vUv*20. + time));
   float sinePulse = (1. + sin(vUv.x*50. - time))*0.5;
   gl_FragColor = vec4( vUv,0.,1. );
   gl_FragColor = vec4( sinePulse,0.,0.,1. );
